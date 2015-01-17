988ad2952ee8850a8aef07ce861b84b59abed78d
luckysahani <luckythegreat4@gmail.com>
2015-01-17 20:29:39 +0530

    Added operators

--- lexer.py
+++ lexer.py
@@ -95,7 +95,7 @@ t_ignore  = ' \t'
 # Error handling rule
 def t_error(t):
     print "Illegal character '%s'" % t.value[0]
-    t.lexer.skip(1)
+    t.lexer.skip(1) 
 
 # Build the lexer
 lexer = lex.lex()
