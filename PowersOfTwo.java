cd352b266f0f14256920eae149a47a311d7292f6
Lucky Sahani <luckysahani@users.noreply.github.com>
2015-01-30 19:24:37 +0530

    Update PowersOfTwo.java

--- PowersOfTwo.java
+++ PowersOfTwo.java
@@ -1,33 +1,3 @@
-/*************************************************************************
- *  Compilation:  javac PowersOfTwo.java
- *  Execution:    java PowersOfTwo N
- *  
- *  This program takes a command-line argument N and prnts a table of
- *  the powers of 2 that are less than or equal to 2^N.
- *
- *  % java PowersOfTwo 5
- *  0 1
- *  1 2
- *  2 4
- *  3 8
- *  4 16
- *  5 32
- *
- *  % java PowersOfTwo 6
- *  0 1
- *  1 2
- *  2 4
- *  3 8
- *  4 16
- *  5 32
- *  6 64
- *
- *  Remarks
- *  ------------
- *  Only works if 0 <= N < 31 since 2^31 overflows an int.
- *
- *************************************************************************/
-
 public class PowersOfTwo {
     public static void main(String[] args) {
 
