403b8fde72bff2a66940af83e1f194d3bcb2561b
Lucky Sahani <luckysahani@users.noreply.github.com>
2015-01-30 19:19:55 +0530

    Update DivisorPattern.java

--- DivisorPattern.java
+++ DivisorPattern.java
@@ -1,35 +1,3 @@
-/*************************************************************************
- *  Compilation:  javac DivisorPattern.java
- *  Execution:    java DivisorPattern N
- *  
- *  Prints a table where entry (i, j) is a '* ' if i divides j
- *  or j divides i and '. ' otherwise.
- *
- *
- *  % java DivisorPattern 20
- *  * * * * * * * * * * * * * * * * * * * * 1
- *  * *   *   *   *   *   *   *   *   *   * 2
- *  *   *     *     *     *     *     *     3
- *  * *   *       *       *       *       * 4
- *  *       *         *         *         * 5
- *  * * *     *           *           *     6
- *  *           *             *             7
- *  * *   *       *               *         8
- *  *   *           *                 *     9
- *  * *     *         *                   * 10
- *  *                   *                   11
- *  * * * *   *           *                 12
- *  *                       *               13
- *  * *         *             *             14
- *  *   *   *                   *           15
- *  * *   *       *               *         16
- *  *                               *       17
- *  * * *     *     *                 *     18
- *  *                                   *   19
- *  * *   * *         *                   * 20
- *
- *************************************************************************/
-
 public class DivisorPattern {
 
     public static void main(String[] args) { 
